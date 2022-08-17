1026f4495d733184642504063834807198add8a6
cathy <cathy@bubulazi.com>
2022-08-17 14:16:06 +0100

    Proyecto terminado
    Implementado el botón de subir likes...

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        postID: '2435667'
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +16,8 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        postID: '34455465'
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +26,53 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        postID: '785649'
     }
 ]
+
+const contenido = document.querySelector('#contenido')
+
+function generarHTML({name, username,location,avatar,post,comment,likes,postID}) {
+    return ` 
+    <article>
+        <header class="autor">
+            <img  class="avatar"  src="./${avatar}" alt="avatar">
+            <div>
+                <h3>${name}</h3>
+                <p>${location}</p>
+            </div>
+        </header>
+        <img class="picture" src="./${post}" alt="Selfi del autor">
+        <div class="likes">
+            <img data-postID="${postID}" id="like-bt" class="iconos" src="./images/icon-heart.png" alt="heart">
+            <img class="iconos" src="./images/icon-comment.png" alt="comment">
+            <img class="iconos" src="./images/icon-dm.png" alt="message">
+        </div>
+        <div class="comentarios">
+            <p><span class="gordita">${likes}</span> likes</p>
+            <p><span class="gordita">${username}</span> ${comment}</p>
+        </div>
+    </article>`
+} 
+
+
+(function displayPosts() {
+    const generarPosts = posts.map( post => generarHTML(post)).join('')
+    contenido.innerHTML = generarPosts
+    function manejarLikes() {
+        const heartColection = document.querySelectorAll('#like-bt')
+        heartColection.forEach( heart => heart.addEventListener('click', () => {
+            const postID = heart.getAttribute('data-postID')
+    
+            const postEncontrado = posts.find( post => post.postID === postID )
+            postEncontrado.likes = postEncontrado.likes + 1
+            console.log(postEncontrado.likes)
+            displayPosts()
+        }))
+    }
+    manejarLikes()
+})()
+
+
 
